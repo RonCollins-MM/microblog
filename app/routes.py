b774a6af241f6043db4c2bc3764f4426cb5e8a99
RonCollins-MM <muriukiron@gmail.com>
2023-05-24 00:20:45 +0300

    implement templates and template inheritance

--- app/routes.py
+++ app/routes.py
@@ -3,9 +3,21 @@
 """Defines app routes"""
 
 from app import app
+from flask import render_template
 
 @app.route('/')
 @app.route('/index')
 def index():
     """Handler for home page"""
-    return 'Hello World !'
+    user = {'username': 'Roncollins'}
+    posts = [
+        {
+            'author': {'username': 'muelsy'},
+            'body': 'To be or not to be ....'
+        },
+        {
+            'author': {'username': 'Dr. Suess'},
+            'body': 'The cat sat on the mat'
+        }
+    ]
+    return render_template('index.html', user=user, title='Home', posts=posts)
