0a108230bb86e07e1ca6a358c65282940c7ad736
RonCollins-MM <muriukiron@gmail.com>
2023-05-29 13:46:45 +0300

    feat: add new model `Post`
    Also add:
    * `One-to-Many` relationship between `User` and `Post`
    * Migrations for model

--- app/models.py
+++ app/models.py
@@ -3,6 +3,10 @@
 """Contains model classes for the blog app"""
 
 from app import db
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
 
 class User(db.Model):
     """Represents users of the blog app"""
@@ -11,8 +15,26 @@ class User(db.Model):
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+    posts = db.relationship('Post', backref='author', lazy='dynamic')
 
     def __repr__(self):
         """Internal representation of objects of this class"""
 
         return f'[User] (username={self.username} email={self.email})'
+
+class Post(db.Model):
+    """Represents a Blog post created by User of blog app"""
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(64))
+    body = db.Column(db.String(140))
+
+    # datetime.utcnow is passed without '()' because the function itself
+    # is passed and not the result of calling it
+    created_at = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        """Internal representation of objects of this class"""
+
+        return f'[Post] (title={self.title} created_by={self.user_id})'
