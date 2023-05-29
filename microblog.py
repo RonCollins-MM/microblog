5cdcdd8828810c633542a6b316caf4194cff8224
RonCollins-MM <muriukiron@gmail.com>
2023-05-29 13:44:56 +0300

    feat: add shell context creator function
    Imports all necessary models and the database instance.
    This ensures that the developer can start testing out models
    using `flask shell` environment.

--- microblog.py
+++ microblog.py
@@ -2,4 +2,24 @@
 
 """Main module"""
 
-from app import app
+from app import app, db
+from app.models import User, Post
+
+# Decorator ensures that this function is invoked whenever `flask shell`
+# is typed in a terminal session
+@app.shell_context_processor
+def create_shell_context():
+    """Creates a context for flask shell session.
+
+    Imports the database instance as well as all the models needed.
+    This ensures that the developer has everything setup to begin testing out
+    the models in the shell session created.
+    """
+
+    context = {
+        'db': db,
+        'User': User,
+        'Post': Post,
+    }
+
+    return context
